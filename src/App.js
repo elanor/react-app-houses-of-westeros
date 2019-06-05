eaa7c84c2302f9d6d89d6d98af881b8bbf62050e
Marina <elanor.me@gmail.com>
2019-06-05 22:05:46 +0300

    Added new project to Github

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logogot2.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This page shows some great houses of the World of Ice and Fire
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
