dc1f60e8c93dc7786a3ff93e599217bfa847d40e
Marina <elanor.me@gmail.com>
2019-06-19 11:43:46 +0300

    Images broken all

--- src/App.js
+++ src/App.js
@@ -5,18 +5,24 @@ import "./styles.css";
 import Lord from "./components/Lord";
 import House from "./components/House";
 import { lords, houses } from "./components/data";
-import { Card, ListGroup, ListGroupItem, Container, Row } from "react-bootstrap";
+import { Card, ListGroup, ListGroupItem} from "react-bootstrap";
 
 const Home = () => (
-  <Card style={{ width: "25rem" }}>
+  <Card style={{ width: "25rem"}}>
     <Card.Body>
       <Card.Title>Welcome to the Houses of Westeros</Card.Title>
       <Card.Subtitle className="mb-2 text-muted">
         You can browse through lords or houses
       </Card.Subtitle>
       <Card.Text>
-        Some quick example text to build on the card title and make up the bulk
-        of the card's content.
+        Great houses are the most powerful members of nobility of the Seven
+        Kingdoms, often being former royalty and now sworn directly to the Iron
+        Throne. Westeros is one of the four known continents in the known world,
+        the others being Essos, Sothoryos, and Ulthos. Most of the area of
+        Westeros is covered by a political entity known as the Seven Kingdoms,
+        while the far north beyond the Wall includes the free folk. The closest
+        foreign nations to Westeros are the Free Cities, a collection of
+        independent city-states across the narrow sea in western Essos.
       </Card.Text>
       <Link to="/lords">Go to lords</Link>
       <Link to="/houses">Go to Houses</Link>
@@ -30,12 +36,16 @@ const LordsList = () => (
     {lords.map(lord => (
       <ListGroup className="list-group-flush">
         <ListGroupItem>
-          <Link to={`/lords/${lord.id}`} className="linkStyle">{lord.name}</Link>
+          <Link to={`/lords/${lord.id}`} className="linkStyle">
+            {lord.name}
+          </Link>
         </ListGroupItem>
       </ListGroup>
     ))}
     <p>
-      <Link to="/lords/all" className="linkStyle">All lords</Link>
+      <Link to="/lords/all">
+        <h4>All lords</h4>
+      </Link>
     </p>
   </p>
 );
@@ -50,12 +60,16 @@ const Houses = () => (
     {houses.map(house => (
       <ListGroup className="list-group-flush">
         <ListGroupItem>
-          <Link to={`/houses/${house.id}`} className="linkStyle">{house.name}</Link>
+          <Link to={`/houses/${house.id}`} className="linkStyle">
+            {house.name}
+          </Link>
         </ListGroupItem>
       </ListGroup>
     ))}
     <p>
-      <Link to="/houses/all" className="linkStyle">All houses</Link>
+      <Link to="/houses/all">
+        <h4>All houses</h4>
+      </Link>
     </p>
   </p>
 );
@@ -64,9 +78,15 @@ const App = () => (
   <Router>
     <div className="App">
       <header>
-        <Link to="/" className="linkStyle">Home</Link>
-        <Link to="/lords" className="linkStyle">Lords</Link>
-        <Link to="/houses" className="linkStyle">Houses</Link>
+        <Link to="/" className="linkStyle">
+          Home
+        </Link>
+        <Link to="/lords" className="linkStyle">
+          Lords
+        </Link>
+        <Link to="/houses" className="linkStyle">
+          Houses
+        </Link>
       </header>
       <Route path="/" exact component={Home} />
       <Route path="/lords/all" component={LordCards} />
--- src/components/House.js
+++ src/components/House.js
@@ -1,14 +1,11 @@
 import React from "react";
 import { houses } from "../components/data";
 import { Card } from "react-bootstrap";
-/* import { Link } from "react-router-dom"; */
-
-/* const findLordById = id => houses.find(house => house.id === id); */
 
 const House = ({ match, house }) => {
   house = house || houses.find(house => house.id === match.params.id);
   if (!house) return null;
-  /* const lords = house.lords.map(findLordById); */
+
   return (
     <Card style={{ width: "18rem" }}>
       <Card.Img variant="top" src={house.image} />
@@ -19,8 +16,6 @@ const House = ({ match, house }) => {
         <Card.Text>{house.description}</Card.Text>
       </Card.Body>
     </Card>
-
-    
   );
 };
 
